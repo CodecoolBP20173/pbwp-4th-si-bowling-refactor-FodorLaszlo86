c5dc5dc1d1105172d8835dc7263d1e848d158d18
Fodor Laszlo <fodor.laszlo86@gmail.com>
2017-11-20 14:52:13 +0100

    get_value function moved before the score function

--- bowling.py
+++ bowling.py
@@ -1,3 +1,19 @@
+
+def get_value(char):
+    if char == '1' or char == '2' or char == '3' or \
+       char == '4' or char == '5' or char == '6' or \
+       char == '7' or char == '8' or char == '9':
+        return int(char)
+    elif char == 'X' or char == 'x':
+        return 10
+    elif char == '/':
+        return 10
+    elif char == '-':
+        return 0
+    else:
+        raise ValueError()
+
+
 def score(game):
     result = 0
     frame = 1
@@ -28,16 +44,3 @@ def score(game):
             frame += 1
     return result
 
-def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
-    else:
-        raise ValueError()
