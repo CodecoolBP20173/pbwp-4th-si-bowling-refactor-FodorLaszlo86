046675acc075786669a8ac9a6ab8147725b1c64c
Fodor Laszlo <fodor.laszlo86@gmail.com>
2017-11-23 15:21:45 +0100

    simplified code for getting scoring in function get_value

--- bowling.py
+++ bowling.py
@@ -1,8 +1,6 @@
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char != '0' and char.isdigit():
         return int(char)
     elif char == 'X' or char == 'x':
         return 10
