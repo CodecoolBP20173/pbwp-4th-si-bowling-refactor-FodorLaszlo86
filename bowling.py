a61da740ce438a3249d4d5a354e949d74d29f0fd
Fodor Laszlo <fodor.laszlo86@gmail.com>
2017-11-23 15:25:39 +0100

    Magic number: 10 assigned to variables max_value and max_frame

--- bowling.py
+++ bowling.py
@@ -16,18 +16,20 @@ def score(game):
     result = 0
     frame = 1
     in_first_roll = True
+    max_value = 10
+    max_frame = 10
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last_score
+            result += max_value - last_score
         else:
             result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
+        if frame < max_frame and get_value(game[i]) == max_value:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                    result += max_value - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last_score = get_value(game[i])
